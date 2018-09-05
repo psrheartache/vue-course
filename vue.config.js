7650bc1fb1303331533d795c170a3ce2144a8f4e
psrheartache <chenmr.cyf@qq.com>
2018-09-05 14:46:19 +0800

    测试提交

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,10 @@ const resolve = dir => path.join(__dirname, dir)
 
 const BASE_URL = process.env.NODE_ENV === 'procution' ? '/iview-admin' : '/'
 
+//去console插件
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+//gzip压缩插件
+// const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 module.exports = {
   lintOnSave: false,
@@ -12,11 +16,21 @@ module.exports = {
     config.resolve.alias
       .set('@',resolve('src'))  //用来定义到Src文件夹的路径
       .set('_c',resolve('src/components')) //到组件
+      .set('assets',resolve('src/assets'))
+      // .set('components',resolve('src/components'))
   },
   //打包时不生成map文件
   productionSourceMap: false,
   //跨域代理
   devServer:{
-    proxy: 'http://localhost:6000'
+
+    // host: '0.0.0.0',
+    // port: 8080,
+    // https: false,
+    // hotOnly: false,
+    // proxy: null, // string | Object
+
+    proxy: 'http://localhost:6000',
+    port:'7777',
   }
 }
